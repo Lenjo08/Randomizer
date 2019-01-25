b63513e98ad221a35e03bfece45b44cef2cfb51f
TypicalGeek <plenjo15@gmail.com>
2019-01-25 15:45:46 +0300

    Removed recursive function
    Also  impleented complete structure

--- randomizer.kt
+++ randomizer.kt
@@ -3,21 +3,44 @@ import java.util.Scanner;
 
 val getKeyboard = Scanner(System.`in`);
 val randomize = Random();
-fun main(args: Array<String>){
-    var chooser:Int;
 
+fun main(args: Array<String>){
     println("Kotlin Randomizer 1.0");
-    println(showMenu("main"));
-    chooser = getKeyboard.nextInt();
-    getAction("main", chooser);
-    println("You chose $chooser");
+    showMenu("main");
+    getAction("main", getKeyboard.nextInt());
 }
 
 fun showMenu(context: String) = when(context){
     "main" -> println("Main Menu\nPlease select an item:\n1: Flip a coin\n2: Roll dice\n3: Pick a card");
+    "coins", "dice", "cards" -> println("How many $context?");
     else -> println("This feature is currently unavailable");
 }
 
-fun getAction(context: String, position: Int){
-    println("You chose $position in $context");
+fun getAction(context: String, position: Int) = when(context){
+    "main" -> when(position){
+        1 -> {
+            var subcontext = "coins";
+            showMenu(subcontext);
+            doAction(subcontext, getKeyboard.nextInt());
+        }
+        2 -> {
+            var subcontext = "dice";
+            showMenu(subcontext);
+            doAction(subcontext, getKeyboard.nextInt());
+        }
+        3 -> {
+            var subcontext = "cards";
+            showMenu(subcontext);
+            doAction(subcontext, getKeyboard.nextInt());
+        }
+        else -> println("This feature is currently unavilable");
+    }
+    else -> println("This feature is currently unavilable");
+}
+
+fun doAction(context: String, num: Int) = when(context){
+    "coins" -> println("Do $num coin stuff"); // Boolean
+    "dice" -> println("Do $num dice stuff"); // Int 1-6
+    "cards" -> println("Do $num card stuff"); // 2D Array
+    else -> println("This feature is currently unavilable");
 }
